97fd36e1ad20f5a900c4adb8b0583782019c6688
asyrafdonison <145311196+asyrafdonison@users.noreply.github.com>
2024-06-24 22:16:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,44 @@
-<?php
-// Mulai session
-session_start();
-
-// Periksa apakah pengguna sudah login
-if (isset($_SESSION['user_id'])) {
-    // Jika sudah, alihkan ke halaman dashboard
-    header("Location: dashboard.php");
-    exit();
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <title>QR Generator</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
 
-    <title>Login</title>
 </head>
 
-<body>
-
-    <section class="bg-gray-50 dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img class="w-8 h-8 mr-2" src="./img/logos_barcodegenerator.png" alt="logo">
-                Barcode Generator
-            </a>
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign in to your account
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="login_process.php" method="POST">
-                        <div>
-                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                username</label>
-                            <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="">
-                        </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                        </div>
-                        <button type="submit" class="bg-yellow-500 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Sign
-                            in</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="./daftar.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                        </p>
-                    </form>
-                </div>
+<body class="bg-white font-sans">
+    <div class="container mx-auto p-4">
+        <div class="flex justify-between items-center">
+            <div class="flex items-center">
+                <img src="./asset/Logo.png" alt="QR Generator Icon">
+            </div>
+            <div class="flex flex-col md:flex-row justify-betwen m-4">
+                <a href="login.php" class="border-solid border-4 border-blue-500 text-blue-500 py-1 px-4 rounded mx-2 my-2">Log In</a>
+                <a href="daftar.php" class="bg-green-500 text-white py-2 px-4 rounded mx-2 my-2">Register</a>
             </div>
         </div>
-    </section>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+        <div class="flex flex-col md:flex-row justify-around items-start mt-10 md:mt-36 ">
+            <div class="flex flex-col max-w-lg">
+                <h1 class="text-6xl font-bold text-blue-500 mt-4">QR<span class="text-green-500 text-5xl">Generator</span></h1>
+                <p class="mt-4 text-gray-700 font-bold">
+                Buat QR Code dengan mudah dan praktis menggunakan QR-Generator!. Anda bisa menciptakan kode QR yang stylish dan informatif dalam hitungan detik. 
+                QR-Generator memudahkan Anda untuk mengubah informasi menjadi QR Code menarik secara praktis, sederhana, dan aman. 
+                </p>
+                <button onclick="window.location.href='./login.php';" class="mt-6 bg-green-500 text-white py-2 px-6 rounded-full w-full">GET STARTED</button>
+            </div>
+
+            <div class="item-center mt-4">
+                <img src="./asset/phone.png" alt="Illustrative Image" class="w-50 h-auto">
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
