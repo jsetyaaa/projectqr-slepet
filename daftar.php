cc45a7181878678db54b87a05927d81f34778cb0
jsetyaaa <jsetyaaa@gmail.com>
2024-06-25 14:15:03 +0700

    Update UI by FrontEnd

--- daftar.php
+++ daftar.php
@@ -25,36 +25,36 @@ if (isset($_SESSION['user_id'])) {
 
 <body>
 
-    <section class="bg-gray-50 dark:bg-gray-900">
+    <section class="bg-gray-50">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                <img class="w-8 h-8 mr-2" src="./img/logos_barcodegenerator.png" alt="logo">
-                Barcode Generator
+            
+            <a href="index.php" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img src="./asset/Logo.png" alt="QR Generator Logo" class="mx-auto"> <!-- Added mx-auto class -->
             </a>
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="w-full bg-white rounded-md shadow-2xl border-black mt-0 sm:max-w-md xl:p-0 dark:bg-white-500 dark:border-gray-900">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign up an account
+                    <h1 class="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-black">
+                        Daftar Akun Baru
                     </h1>
                     <form class="space-y-4 md:space-y-6" action="register.php" method="POST">
                         <div>
-                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your
                                 username</label>
-                            <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="">
+                            <input type="username" name="username" id="username" class="w-96 p-3 border border-black" placeholder="username" required="">
                         </div>
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your
                                 email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            <input type="email" name="email" id="email" class="w-96 p-3 border border-black" placeholder="name@company.com" required="">
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="w-96 p-3 border border-black" required="">
                         </div>
-                        <button type="submit" class="bg-yellow-500 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Sign
-                            Up</button>
+                        <button type="submit" class="w-96 bg-green-500 text-white p-3 rounded-md"> Daftar Akun</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Have an account? <a href="./index.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</a>
+                            Sudah punya akun? <a href="./login.php" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Masuk</a>
                         </p>
                     </form>
                 </div>
